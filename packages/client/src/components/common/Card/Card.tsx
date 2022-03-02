1ce7fbd10cd92cdb007defcc5b18a79842d19315
Simon <62551180+simonagleev@users.noreply.github.com>
2022-03-02 05:32:54 -0800

    seven-card-dashboard

--- packages/client/src/components/common/Card/Card.tsx
+++ packages/client/src/components/common/Card/Card.tsx
@@ -1,33 +1,109 @@
 import * as React from 'react';
 
-import { makeStyles } from '@material-ui/core';
+import { Avatar, Chip, makeStyles, Box, Typography, colors, useTheme } from '@material-ui/core';
 
 import Paper from '@material-ui/core/Paper';
+import Stack from '@mui/material/Stack';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
 
 const useStyles = makeStyles({
     root: {
         position: 'relative',
         height: '100%',
         width: '100%',
-        overflow: 'hidden',
-        background: 'gainsboro',
+        display: 'flex',
+        alignItems: 'stretch',
+        justifyContent: 'stretch'
     },
+    container: {
+        flex: 1,
+    },
+
 });
 
-interface ICardProps {
-    children?: React.ReactNode;
+interface IValue {
+    label: string;
+    color: string;
 }
 
-export const Card = ({
-    children = null,
+interface ICardProps {
+    idx?: number;
+    obj?: Record<string, IValue>;
+}
+
+export const Card2 = ({
+    idx,
+    obj = {},
 }: ICardProps) => {
     const classes = useStyles();
+    const theme = useTheme();
+    const {
+        label,
+        color
+    } = obj[`value${idx}`];
+
+    // const obj1 = { test: 1}
+    // obj1.test
+    // obj1["test"]
+
     return (
         <Paper className={classes.root}>
-            {children}
+            <Stack
+                className={classes.container}
+                direction="column"
+                justifyContent="center"
+                alignItems="stretch"
+                spacing={2}
+            >
+                <Stack 
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"                   
+                >
+                    <Avatar>
+                        <PointOfSaleIcon color='error'/>
+                    </Avatar>
+                </Stack>
+                
+                <Stack
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={0}
+                    sx={{
+                        '& .MuiChip-outlined': {
+                            border: `1px solid ${color}`
+                        }
+                    }}
+                 >
+                    <Box
+                        style={{
+                            flex: 1,
+                            height: '2px',
+                            background: color,
+                        }}
+                    />
+                    <Chip  variant="outlined" label={label} style={{color}} />
+                    <Box
+                        style={{
+                            flex: 1,
+                            height: '2px',
+                            background: color,
+                        }}
+                    />
+                </Stack>
+                <Stack 
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Typography variant='h4' color='secondary'>15</Typography>
+                </Stack>
+                
+            </Stack>
         </Paper>
     );
 };
 
-export default Card;
+export default Card2;
 
--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -10,57 +10,68 @@ const CC_FULL_HEIGHT = "calc(100vh - 80px)";
 const CC_CELL_PADDING = "7px";
 const CC_MIN_HEIGHT = "635px";
 
+
+interface IObj {
+  id?: number
+
+  text?: string
+
+}
+
+const createCard = (idx: number, columns = "6"): TypedField => ({
+  type: FieldType.Hero,
+  columns,
+  height: `33vh`,
+  right: CC_CELL_PADDING,
+  bottom: '10px',
+  child: {
+    type: FieldType.Component,
+    element: (obj) => <Card obj={obj} idx={idx} />
+  },
+})
+
 const fields: TypedField[] = [
-  {
-    type: FieldType.Hero,
-    columns: "12",
-    tabletColumns: "6",
-    desktopColumns: "7",
-    height: `max(${CC_FULL_HEIGHT}, ${CC_MIN_HEIGHT})`,
-    phoneHeight: `max(${CC_MIN_HEIGHT}, 30vh)`,
-    desktopRight: CC_CELL_PADDING,
-    tabletRight: CC_CELL_PADDING,
-    phoneBottom: CC_CELL_PADDING,
-    child: {
-      type: FieldType.Component,
-      element: () => <Card />
-    },
-  },
-  {
-    type: FieldType.Group,
-    fieldBottomMargin: '0',
-    fieldRightMargin: '0',
-    columns: '12',
-    tabletColumns: "6",
-    desktopColumns: '5',
-    fields: [
-      {
-        type: FieldType.Hero,
-        height: `calc(${CC_FULL_HEIGHT} / 2)`,
-        phoneHeight: `max(calc(${CC_MIN_HEIGHT} / 2), 30vh)`,
-        minHeight: `calc(${CC_MIN_HEIGHT} / 2)`,
-        bottom: CC_CELL_PADDING,
-        child: {
-          type: FieldType.Component,
-          element: () => <Card />
-        },
-      },
-      {
-        type: FieldType.Hero,
-        height: `calc(${CC_FULL_HEIGHT} / 2)`,
-        phoneHeight: `max(calc(${CC_MIN_HEIGHT} / 2), 30vh)`,
-        minHeight: `calc(${CC_MIN_HEIGHT} / 2)`,
-        child: {
-          type: FieldType.Component,
-          element: () => <Card />,
-        },
-      },
-    ],
-  },
+  createCard(0),
+  createCard(1),
+  createCard(2, "4"),
+  createCard(3, "4"),
+  createCard(4, "4"),
+  createCard(5),
+  createCard(6),
 ];
 
 export const HomePage = () => (
   <One
+    handler={() => ({
+      value0: {
+        label: "1",
+        color: "green",
+      },
+      value1: {
+        label: "2",
+        color: "red",
+      },
+      value2: {
+        label: "3",
+        color: "green",
+      },
+      value3: {
+        label: "4",
+        color: "green",
+      },
+      value4: {
+        label: "5",
+        color: "green",
+      },
+      value5: {
+        label: "6",
+        color: "green",
+      },
+      value6: {
+        label: "7",
+        color: "green",
+      },
+    })}
     fields={fields}
   />
 );
